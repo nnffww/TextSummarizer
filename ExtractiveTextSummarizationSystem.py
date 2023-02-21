15e6389527136320b0e294d10d1810f46f8db760
nnffww <86704794+nnffww@users.noreply.github.com>
2023-02-22 03:56:41 +0800

    Update ExtractiveTextSummarizationSystem.py

--- ExtractiveTextSummarizationSystem.py
+++ ExtractiveTextSummarizationSystem.py
@@ -74,7 +74,7 @@ if choice == 'News Article':
                         file_name = 'PoliticsArticle.csv',
                         mime = 'text/csv')
    if option == 'Sport':
-      url = 'https://raw.githubusercontent.com/faraawaheeda/streamlitProject/main/business%20data.csv?token=GHSAT0AAAAAAB44S4MWPQE3QEME6JT4YSV4Y6Q4XLQ'
+      url = 'https://raw.githubusercontent.com/nnffww/TextSummarizer/main/sport%20_data.csv'
       df = pd.read_csv(url,encoding="latin-1")
       st.write(df.head(50))
       st.download_button("Download",
@@ -82,7 +82,7 @@ if choice == 'News Article':
                         file_name = 'SportArticle.csv',
                         mime = 'text/csv')
    if option == 'Technology':
-      url = 'https://raw.githubusercontent.com/faraawaheeda/streamlitProject/main/business%20data.csv?token=GHSAT0AAAAAAB44S4MWPQE3QEME6JT4YSV4Y6Q4XLQ'
+      url = 'https://raw.githubusercontent.com/nnffww/TextSummarizer/main/technology%20_data.csv'
       df = pd.read_csv(url,encoding="latin-1")
       st.write(df.head(50))
       st.download_button("Download",

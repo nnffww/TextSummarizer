439c2c79619d993abaeede5735157eb83905bdde
nnffww <86704794+nnffww@users.noreply.github.com>
2023-02-22 02:40:20 +0800

    Update ExtractiveTextSummarizationSystem.py

--- ExtractiveTextSummarizationSystem.py
+++ ExtractiveTextSummarizationSystem.py
@@ -52,18 +52,18 @@ if choice == 'News Article':
    if option == 'Business':
       url = 'https://raw.githubusercontent.com/nnffww/TextSummarizer/main/business%20_data.csv'
       df = pd.read_csv(url,encoding="latin-1")
-      st.write(df.head(20))
+      st.write(df.head(31))
       st.download_button("Download",
                         df.to_csv(),
                         file_name = 'BusinessArticle.csv',
                         mime = 'text/csv')
    if option == 'Entertaiment':
-      url = 'https://raw.githubusercontent.com/faraawaheeda/streamlitProject/main/business%20data.csv?token=GHSAT0AAAAAAB44S4MWPQE3QEME6JT4YSV4Y6Q4XLQ'
+      url = 'https://raw.githubusercontent.com/nnffww/TextSummarizer/main/entertaiment%20_data.csv'
       df = pd.read_csv(url,encoding="latin-1")
-      st.write(df.head(20))
+      st.write(df.head(31))
       st.download_button("Download",
                         df.to_csv(),
-                        file_name = 'BusinessArticle.csv',
+                        file_name = 'EntertaimentArticle.csv',
                         mime = 'text/csv')
    if option == 'Politics':
       url = 'https://raw.githubusercontent.com/faraawaheeda/streamlitProject/main/business%20data.csv?token=GHSAT0AAAAAAB44S4MWPQE3QEME6JT4YSV4Y6Q4XLQ'
